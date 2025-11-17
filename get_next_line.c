7ce3230fb6cd3fc10d22d7358a9c20475ebbb5e3
Tom Espinosa <toespino@2A1.42angouleme.fr>
2025-11-17 20:00:34 +0100

    changing the GNL project implementation logic

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,43 @@
 /*   By: toespino <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 17:11:08 by toespino          #+#    #+#             */
-/*   Updated: 2025/11/17 02:41:53 by toespino         ###   ########.fr       */
+/*   Updated: 2025/11/17 20:00:02 by toespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
 	char		*out;
 	int			i;
 	static char	*buffer;
-	int			len;
 	char		*temp;
 
-	out =  "";
+	out =  NULL;
 	if (fd < 0 || fd > 1023 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
-	if (!buffer)
+	if (!*buffer)
 		i = read(fd, buffer, BUFFER_SIZE);
 	else
 		i = ft_strlen(buffer);
-	if (!i)
-		return (NULL);
-	len = ft_lenline(buffer);
-	if (len == 0)
+	while (!ft_have_end_line() && i != 0)
 	{
-		buffer = 
+		i = read(fd, temp, BUFFER_SIZE);
+		buffer = ft_strjoin(buffer, temp);
 	}
+	out = ft_strlcpy_until(buffer);
+	if (i != 0)
+	{
+		temp = buffer;
+		free(buffer);
+		buffer = ft_strlcpy_since(temp);
+	}
+	else
+		free(buffer);
+	return (out);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: toespino <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 15:34:00 by toespino          #+#    #+#             */
-/*   Updated: 2025/11/15 15:03:01 by toespino         ###   ########.fr       */
+/*   Updated: 2025/11/17 17:18:37 by toespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,46 +20,6 @@ int	ft_strlen(char *str)
 	while (str[i])
 		i++;
 	return (i);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
-	size_t	srclen;
-	size_t	i;
-
-	srclen = 0;
-	i = 0;
-	while (src[srclen])
-		srclen++;
-	if (size > 0)
-	{
-		while (i < size - 1 && i < srclen)
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
-	}
-	return (srclen);
-}
-
-char	*ft_substr(int start, char *str, int len)
-{
-	char	*out;
-	size_t	lens;
-
-	if (!s)
-		return (NULL);
-	lens = ft_strlen(s);
-	if (start >= lens)
-		return (ft_strdup(""));
-	if (lens - start < len)
-		len = lens - start;
-	out = malloc(len + 1);
-	if (!out)
-		return (NULL);
-	ft_strlcpy(out, &s[start], len + 1);
-	return (out);
 }
 
 char	*ft_strjoin(char *s1, char *s2)
@@ -86,5 +46,6 @@ char	*ft_strjoin(char *s1, char *s2)
 		i++;
 		j++;
 	}
+	free(s1);
 	return (out);
 }
